f857494daf47a86ba73028c74bed27903e820a57
Broofdeath <39744289+Broofdeath@users.noreply.github.com>
2018-06-27 23:35:21 -0400

    Add files via upload
    vista actualizada (le faltaa agregar el diseño correspondiente)

--- application/views/adminBeneficios.php
+++ application/views/adminBeneficios.php
@@ -34,71 +34,66 @@
 		  			<a class="w3-button w3-large w3-circle w3-xlarge w3-white w3-hover-green w3-border" style="z-index:0"><i class="fa fa-trash"></i></a>
 		  		</div>
 		  	</div>
-
-		  	
-
+		  	<style type="text/css">
+		  		.descripcionBeneficio{
+		  			width: 150px;
+		  			height: 150px;
+		  		}
+		  	</style>
 		  	<div class="w3-container w3-row w3-margin-top">
-		  		<div class="w3-col l3 m3 s3">
-		  			<h2>Comestibles</h2>
-		  			<hr>
-		  			<div class="w3-container w3-row w3-margin-top">
-		  				<div class="w3-col l4 m4 s4">
-		  					
-		  				</div>
-		  				<div class="w3-col l8 m8 s8 ">
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>	    				
-		  				</div>
-		  			</div>
-		  		</div>
-		  		<div class="w3-col l3 m3 s3">
-		  			<h2>Hospedaje</h2>
-		  			<hr>
-		  			<div class="w3-container w3-row w3-margin-top">
-		  				
-		  				<div class="w3-col l8 m8 s8 ">
-		  					<i class="fas fa-bed w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-bed w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-bed w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-bed w3-xxlarge">Hooola</i>	  				
-		  				</div>
-		  			</div>
-		  		</div>
-		  		<div class="w3-col l3 m3 s3">
-		  			<h2>Articulos</h2>
-		  			<hr>
-		  			<div class="w3-container w3-row w3-margin-top">
-		  				<div class="w3-col l4 m4 s4">
-		  					
-		  				</div>
-		  				<div class="w3-col l8 m8 s8 ">
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>	  		  				
-		  				</div>
-		  			</div>
-		  		</div>
-		  		<div class="w3-col l3 m3 s3">
-		  			<h2>Impresiones</h2>
-		  			<hr>
-		  			<div class="w3-container w3-row w3-margin-top">
-		  				<div class="w3-col l4 m4 s4">
-		  					
-		  				</div>
-		  				<div class="w3-col l8 m8 s8 ">
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>
-		  					<i class="fas fa-file w3-xxlarge">Hooola</i>	    				
-		  				</div>
-		  			</div>
-		  		</div>
-
+		  		<?php
+		  			foreach ($dataBeneficios as $key => $beneficios) {
+		  				$beneficios = $beneficios->toArray();
+		  				$beneficio = $beneficios['_columns'];
+		  				switch ($beneficio['ben_tipo']) {
+		  					case 1:
+		  						echo "<div class = 'w3-col l3 m3 s3'>";
+						  		echo "<h2>Hospedaje</h2>";
+						  		echo "<hr>";
+						  		echo "<div class = 'w3-container w3-row w3-margin-top' >";
+						  		echo "<div class = 'w3-col l8 m8 s8'>";		
+						  		echo "<fieldset class = 'descripcionBeneficio'>".$beneficio['ben_descripcion']."</fieldset>"; 				
+						  		echo "</div>";
+						  		echo "</div>";
+						  		echo "</div>";
+		  					break;
+		  					case 2:
+		  						echo "<div class = 'w3-col l3 m3 s3' >";
+						  		echo "<h2>Comestible</h2>";
+						  		echo "<hr>";
+						  		echo "<div class = 'w3-container w3-row w3-margin-top' >";
+						  		echo "<div class = 'w3-col l8 m8 s8'>";		
+						  		echo "<fieldset class = 'descripcionBeneficio'>".$beneficio['ben_descripcion']."</fieldset>"; 				
+						  		echo "</div>";
+						  		echo "</div>";
+						  		echo "</div>";
+		  					break;
+		  					case 3:
+		  						echo "<div class = 'w3-col l3 m3 s3' >";
+						  		echo "<h2>Articulo</h2>";
+						  		echo "<hr>";
+						  		echo "<div class = 'w3-container w3-row w3-margin-top' >";
+						  		echo "<div class = 'w3-col l8 m8 s8'>";		
+						  		echo "<fieldset class = 'descripcionBeneficio'>".$beneficio['ben_descripcion']."</fieldset>"; 				
+						  		echo "</div>";
+						  		echo "</div>";
+						  		echo "</div>";
+		  					break;
+		  					case 4:
+		  						echo "<div class = 'w3-col l3 m3 s3' >";
+						  		echo "<h2>Impresiones</h2>";
+						  		echo "<hr>";
+						  		echo "<div class = 'w3-container w3-row w3-margin-top' >";
+						  		echo "<div class = 'w3-col l8 m8 s8'>";		
+						  		echo "<fieldset class = 'descripcionBeneficio'>".$beneficio['ben_descripcion']."</fieldset>"; 				
+						  		echo "</div>";
+						  		echo "</div>";
+						  		echo "</div>";
+		  					break;
+		  				}
+				  	}
+		  		?>
 		  	</div>
-
 			<!-- The Modal -->
 			 <div id="id01" class="w3-modal">
 			    <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
@@ -107,34 +102,49 @@
 			        <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
 			      </div>
 
-			      <form class="w3-container" action="/action_page.php">
+			      <form class="w3-container" method="post" action="<?=site_url('Administrador/ingresarBeneficio');?>">
 			        <div class="w3-section">
 		        		<h2>Añadir Beneficio</h2>
-			          <label><b>Nombre</b></label>
-			          <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese nombre" name="usrname" required>
-
+			          <div class="row">
+			          	<div class="w3-col l6 m6 s6">
+			          		<label>Fecha inicio</label>
+			          		<input type="date" name="beneficio[ben_feInicio]" required>
+			          	</div>
+			          	<div class="w3-col l6 m6 s6">
+			          		<label>Fecha termino</label>
+			          		<input type="date" name="beneficio[ben_feTermino]" required>
+			          	</div>
+			          </div>
 			          <div class="w3-row">
 			          	<div class="w3-col l6 m6 s6">
 			          		<label><b>Tipo Beneficio</b></label>
-					          <select class="w3-input w3-border w3-margin-bottom" name="psw" required>
+					          <select class="w3-input w3-border w3-margin-bottom" name="beneficio[ben_tipo]" required>
 					          	<option value="0" selected disabled>- Seleccione tipo -</option>
 					          	<option value="1">- Hospedaje -</option>
 					          	<option value="2">- Comestible -</option>
-					          	<option value="1">- Articulo -</option>
-					          	<option value="1">- Impresiones -</option>
-					          	
+					          	<option value="3">- Articulo -</option>
+					          	<option value="4">- Impresiones -</option>					          	
 					          </select>	
 			          	</div>
 			          	<div class="w3-col l6 m6 s6">
 			          		<label><b>Costo ICoins</b></label>
-			          		<input class="w3-input w3-border" type="number" placeholder="000000" name="psw" required>
+			          		<input class="w3-input w3-border" type="number" placeholder="000000" name="beneficio[ben_valor]" required>
 			          	</div>
 			          	<div class="w3-col l12 m12 s12">
 		          			<label><b>Descripción</b></label>
-          					<textarea style="width: 100%; height: 100px"></textarea>
+          					<textarea name="beneficio[ben_descripcion]" style="width: 100%; height: 100px"></textarea>
 						</div>
 			          </div>
-			          
+			          <div class="row">
+			          	<div class="w3-col l12 m12 s12">
+			          		<label><b>Sede</b></label>
+					          <select class="w3-input w3-border w3-margin-bottom" name="beneficio[ben_sed_id]" required>
+					          	<option value="0" selected disabled>- Seleccione sede -</option>
+					          	<option value="1">- Renca -</option>
+					          	<option value="2">- Valparaiso -</option>					          	
+					          </select>
+			          	</div>
+			          </div>
 			          <div class="w3-container">
 
 			          	<div class="w3-row">
